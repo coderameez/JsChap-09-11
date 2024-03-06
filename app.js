153b821cae05dda83b62faf55577d9ae628c1405
Muhammad Rameez Khan <m.rameezkhan77@gmail.com>
2024-03-07 01:06:36 +0500

    added index.html and app.js files

---
+++ app.js
@@ -0,0 +1,35 @@
+// Task 1
+var result = prompt("Enter your city");
+if(result == "karachi") {
+    alert("Welcome to the City of Lights");
+}
+
+// Task 2
+var resultOne = prompt("Enter your Gender")
+if(resultOne == "male"){
+    alert("Good Morning Sir")
+}
+else if(resultOne== "female"){
+    alert("Good Morning Mam")
+}
+else{
+    alert("Fuck off there are only 2 genders")
+}
+
+// Task 3
+var signal = prompt("Color on the traffic signal?");
+if(signal == "red") {
+    alert("Must Stop")
+}
+else if(signal == "yellow") {
+    alert("Ready to Move")
+}
+else if (signal == "green"){
+    alert("Move now")
+}
+else(
+    alert("Invalid input")
+)
+// Task 4
+console.log
+
